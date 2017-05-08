d4e422852e8c1b09620c7198317460532286bb75
tao liu <werewaswas@live.com>
2017-05-08 22:13:37 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function nowServing (katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(line) {
+  if (line.length > 0 ) {
+    let lineList = [];
+    let lineListString = ``
+    line.forEach ((element, index) => {
+      lineList.push(`${index + 1}. ${element}`)
+    });
+    lineListString = lineList.join(", ")
+    return `The line is currently: ${lineListString}`;
+  }
+  else {
+    return `The line is currently empty.`
+  }
+ 
+}
