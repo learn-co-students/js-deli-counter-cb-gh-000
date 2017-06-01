b5f59f8823cd6d05805bb49789dde7c09c90e472
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 19:57:24 -0400

    Create function nowServing

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(katzDeliLine, newPersonName) {
   katzDeliLine.push(newPersonName);
   return `Welcome, ${newPersonName}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let personBeingServed = katzDeliLine.shift();
+    return `Currently serving ${personBeingServed}.`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
