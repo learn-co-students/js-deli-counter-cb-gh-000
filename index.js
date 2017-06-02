1fcc1bd1137ca12db824f3bc74179a1ab538291a
Robert Wayne <robertjwayne@gmail.com>
2017-06-01 20:06:04 -0400

    Create function currentLine

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+
+  let lineMembersWithNumbers = [];
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    lineMembersWithNumbers.push(`${i + 1}. ${katzDeliLine[i]}`)
+  }
+
+  return `The line is currently: ${lineMembersWithNumbers.join(", ")}`
+}
